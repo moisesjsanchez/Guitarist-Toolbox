2ce7f5bd6c1f8fa77bd5bdaf9e80c7cdd1befc9d
moisesjsanchez <moijsanchez@gmail.com>
2019-12-28 12:51:08 -0800

    created shell for api

---
+++ Guitarist_toolbox/api/api.py
@@ -0,0 +1,66 @@
+from flask import Flask, jsonify, request
+
+#get all music information
+'''
+@app.route('/music-info', methods=['GET'])
+def get_information():
+    request.get_json()
+    music_info = MusicInfo()
+'''
+
+'''
+@app.route('/music-info/chords', methods=['GET'])
+def get_chords():
+    request.get_json()
+    music_info = MusicInfo()
+'''
+
+'''
+@app.route('/music-info/technique', methods=['GET'])
+def get_chords():
+    request.get_json()
+    music_info = MusicInfo()
+'''
+
+'''
+@app.route('/music-info/time', methods=['GET'])
+def get_chords():
+    request.get_json()
+    music_info = MusicInfo()
+'''
+
+'''
+@app.route('/music-info/scale', methods=['GET'])
+def get_chords():
+    request.get_json()
+    music_info = MusicInfo()
+'''
+
+#get a single musical information
+'''
+@app.route('/music-info/chord/<chords>', methods=['GET'])
+def get_chords():
+    request.get_json()
+    music_info = MusicInfo()
+'''
+
+'''
+@app.route('/music-info/technique'/<technique>', methods=['GET'])
+def get_chords():
+    request.get_json()
+    music_info = MusicInfo()
+'''
+
+'''
+@app.route('/music-info/time/<time>', methods=['GET'])
+def get_chords():
+    request.get_json()
+    music_info = MusicInfo()
+'''
+
+'''
+@app.route('/music-info/scales/<scales>', methods=['GET'])
+def get_chords():
+    request.get_json()
+    music_info = MusicInfo()
+'''
