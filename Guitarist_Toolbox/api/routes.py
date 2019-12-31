a1d961e240c0cd20eedbf370efd4127920e88d69
moisesjsanchez <moijsanchez@gmail.com>
2019-12-31 03:16:56 -0800

    create endpoints for select all function of API

--- Guitarist_Toolbox/api/routes.py
+++ Guitarist_Toolbox/api/routes.py
@@ -5,40 +5,58 @@ from Guitarist_Toolbox.model import Chord,Scale,Time,Technique
 api = Blueprint('api', __name__)
 
 #get all music information
-'''
-@app.route('/music-info', methods=['GET'])
-def get_information():
-    request.get_json()
-    music_info = MusicInfo()
-'''
 
-'''
-@app.route('/music-info/chord', methods=['GET'])
+@api.route('/music-info/chords', methods=['GET'])
 def get_chords():
-    request.get_json()
-    music_info = MusicInfo()
-'''
+    music_info = request.get_json()
+    chords = Chord.query.all()
 
-'''
-@app.route('/music-info/technique', methods=['GET'])
-def get_chords():
-    request.get_json()
-    music_info = MusicInfo()
-'''
+    output = []
+    for chord in chords:
+        all_chords = {}
+        all_chords['chord'] = chord.chord
+        output.append(all_chords)
+    
+    return jsonify({'Chord Progressions': output}), 200
 
-'''
-@app.route('/music-info/time-signature', methods=['GET'])
-def get_chords():
-    request.get_json()
-    music_info = MusicInfo()
-'''
+@api.route('/music-info/scales', methods=['GET'])
+def get_scales():
+    music_info = request.get_json()
+    scales = Scale.query.all()
 
-'''
-@app.route('/music-info/scale', methods=['GET'])
-def get_chords():
-    request.get_json()
-    music_info = MusicInfo()
-'''
+    output = []
+    for scale in scales:
+        all_scales = {}
+        all_scales['scale'] = scale.scale
+        output.append(all_scales)
+    
+    return jsonify({'Scales': output}), 200
+
+@api.route('/music-info/technique', methods=['GET'])
+def get_technique():
+    music_info = request.get_json()
+    techniques = Technique.query.all()
+
+    output = []
+    for technique in techniques:
+        all_technique = {}
+        all_technique['technique'] = technique.technique
+        output.append(all_technique)
+    
+    return jsonify({'Techniques': output}), 200
+
+@api.route('/music-info/time', methods=['GET'])
+def get_time():
+    music_info = request.get_json()
+    times = Time.query.all()
+
+    output = []
+    for time in times:
+        all_times = {}
+        all_times['time_signature'] = time.time_signature
+        output.append(all_times)
+    
+    return jsonify({'Time Signatues': output}), 200
 
 #get a single musical information
 '''
