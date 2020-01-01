b6d48f6fc469ba30d5f80d50c610c6de1f5ffab7
moisesjsanchez <moijsanchez@gmail.com>
2020-01-01 01:49:16 -0800

    finished entire fetch all and fetch by type part of API

--- Guitarist_Toolbox/api/routes.py
+++ Guitarist_Toolbox/api/routes.py
@@ -5,6 +5,39 @@ from Guitarist_Toolbox.model import Chord,Scale,Time,Technique
 api = Blueprint('api', __name__)
 
 #get all music information
+@api.route('/music-info/all', methods=['GET'])
+def get_all():
+    music_info = request.get_json()
+    chords = Chord.query.all()
+    scales = Scale.query.all()
+    techniques = Technique.query.all()
+    times = Time.query.all()
+
+    output = []
+
+    for chord in chords:
+        all_chords = {}
+        all_chords['Chord'] = chord.chord
+        output.append(all_chords)
+
+    for scale in scales:
+        all_scales = {}
+        all_scales['Scale'] = scale.scale
+        output.append(all_scales)
+
+    for technique in techniques:
+        all_technique = {}
+        all_technique['Technique'] = technique.technique
+        all_technique['Type'] = technique.tech_type
+        output.append(all_technique)
+
+    for time in times:
+        all_times = {}
+        all_times['Time Signature'] = time.time_signature
+        all_times['Type'] = time.time_type
+        output.append(all_times)
+    
+    return jsonify({'Whole Toolbox': output}), 200
 
 @api.route('/music-info/chords', methods=['GET'])
 def get_chords():
@@ -14,7 +47,7 @@ def get_chords():
     output = []
     for chord in chords:
         all_chords = {}
-        all_chords['chord'] = chord.chord
+        all_chords['Chord'] = chord.chord
         output.append(all_chords)
     
     return jsonify({'Chord Progressions': output}), 200
@@ -27,7 +60,7 @@ def get_scales():
     output = []
     for scale in scales:
         all_scales = {}
-        all_scales['scale'] = scale.scale
+        all_scales['Scale'] = scale.scale
         output.append(all_scales)
     
     return jsonify({'Scales': output}), 200
@@ -40,31 +73,68 @@ def get_technique():
     output = []
     for technique in techniques:
         all_technique = {}
-        all_technique['technique'] = technique.technique
+        all_technique['Technique'] = technique.technique
+        all_technique['Type'] = technique.tech_type
         output.append(all_technique)
     
     return jsonify({'Techniques': output}), 200
 
 @api.route('/music-info/time', methods=['GET'])
 def get_time():
-    music_info = request.get_json()
     times = Time.query.all()
 
     output = []
     for time in times:
         all_times = {}
-        all_times['time_signature'] = time.time_signature
+        all_times['Time Signature'] = time.time_signature
+        all_times['Type'] = time.time_type
         output.append(all_times)
     
     return jsonify({'Time Signatues': output}), 200
 
-#get a single musical information
-'''
-@app.route('/music-info/chord/<chord>', methods=['GET'])
-def get_chords():
-    request.get_json()
-    music_info = MusicInfo()
-'''
+#get a single musical information by critera
+
+@api.route('/music-info/technique/tech_type/both', methods=['GET'])
+def get_both():
+    current_tech_type = Technique.query.filter_by(tech_type='Both')
+
+    output = []
+
+    for tech_type in current_tech_type:
+        all_technique = {}
+        all_technique['Technique'] = tech_type.technique
+        all_technique['Type'] = tech_type.tech_type
+        output.append(all_technique)                          
+    
+    return jsonify({'Techniques': output}), 200
+
+@api.route('/music-info/technique/tech_type/electric', methods=['GET'])
+def get_electric():
+    current_tech_type = Technique.query.filter_by(tech_type='Electric')
+
+    output = []
+
+    for tech_type in current_tech_type:
+        all_technique = {}
+        all_technique['Technique'] = tech_type.technique
+        all_technique['Type'] = tech_type.tech_type
+        output.append(all_technique)                          
+    
+    return jsonify({'Techniques': output}), 200
+
+@api.route('/music-info/technique/tech_type/acoustic', methods=['GET'])
+def get_acoustic():
+    current_tech_type = Technique.query.filter_by(tech_type='Acoustic')
+
+    output = []
+
+    for tech_type in current_tech_type:
+        all_technique = {}
+        all_technique['Technique'] = tech_type.technique
+        all_technique['Type'] = tech_type.tech_type
+        output.append(all_technique)                          
+    
+    return jsonify({'Techniques': output}), 200
 
 '''
 @app.route('/music-info/technique'/<technique>', methods=['GET'])
@@ -72,6 +142,8 @@ def get_chords():
     request.get_json()
     music_info = MusicInfo()
 '''
+
+#get random music information based on column selected
 
 '''
 @app.route('/music-info/time-signature/<time-signature>', methods=['GET'])
