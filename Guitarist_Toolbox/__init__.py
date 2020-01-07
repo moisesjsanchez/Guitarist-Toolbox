e66a8bf5d86411f8907a343a48581549d947645e
moisesjsanchez <moijsanchez@gmail.com>
2020-01-07 02:19:45 -0800

    inserted input variable assignment

--- Guitarist_Toolbox/__init__.py
+++ Guitarist_Toolbox/__init__.py
@@ -1,7 +1,6 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 from Guitarist_Toolbox.config import DevelopmentConfig
 
 db = SQLAlchemy()
@@ -10,7 +9,7 @@ migrate = Migrate()
 
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
-    app.config.from_object(DevelopmentConfig)
+    app.config.from_object(config_class)
     db.init_app(app)
     migrate.init_app(app, db)
 
