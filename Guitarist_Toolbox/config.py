a972918182eae165ac642c9f9198573036ed0f7b
moisesjsanchez <moijsanchez@gmail.com>
2020-01-07 02:21:07 -0800

    variable test fixed to allow change when using different config

--- Guitarist_Toolbox/config.py
+++ Guitarist_Toolbox/config.py
@@ -6,11 +6,11 @@ class BaseConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = my_postgres
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TESTING = False
 
 
 class TestConfig(BaseConfig):
     TESTING = True
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test_app.db')
 
 
 class DevelopmentConfig(BaseConfig):
