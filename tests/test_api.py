297db7cb79e87cae2b22cd47334c7cef89b06c28
moisesjsanchez <moijsanchez@gmail.com>
2020-01-10 23:38:01 -0800

    completed unit testing for API for all routes

--- tests/test_api.py
+++ tests/test_api.py
@@ -27,29 +27,45 @@ class APITestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_get_technique(self):
-        response = self.client.get('/music-info/technique')
+        response = self.client.get('/music-info/techniques')
         self.assertEqual(response.status_code, 200)
 
     def test_get_tech_both(self):
-        response = self.client.get('/music-info/technique/tech_type/both')
+        response = self.client.get('/music-info/techniques/both')
         self.assertEqual(response.status_code, 200)
 
     def test_get_tech_electric(self):
-        response = self.client.get('/music-info/technique/tech_type/electric')
+        response = self.client.get('/music-info/techniques/electric')
         self.assertEqual(response.status_code, 200)
 
     def test_get_tech_acoustic(self):
-        response = self.client.get('/music-info/technique/tech_type/acoustic')
+        response = self.client.get('/music-info/techniques/acoustic')
         self.assertEqual(response.status_code, 200)
 
     def test_get_tech_simple(self):
-        response = self.client.get('/music-info/time/time_type/simple')
+        response = self.client.get('/music-info/times/simple')
         self.assertEqual(response.status_code, 200)
 
     def test_get_tech_compound(self):
-        response = self.client.get('/music-info/time/time_type/compound')
+        response = self.client.get('/music-info/times/compound')
         self.assertEqual(response.status_code, 200)
 
     def test_get_tech_complex(self):
-        response = self.client.get('/music-info/time/time_type/complex')
+        response = self.client.get('/music-info/times/complex')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_chord_random(self):
+        response = self.client.get('/music-info/chords/random')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_scale_random(self):
+        response = self.client.get('/music-info/scales/random')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_technique_random(self):
+        response = self.client.get('/music-info/techniques/random')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_time_random(self):
+        response = self.client.get('/music-info/times/random')
         self.assertEqual(response.status_code, 200)
