a63349e3495f9cd44b63f5a59f637ec13ec81b9c
moisesjsanchez <moijsanchez@gmail.com>
2020-01-11 01:50:35 -0800

    Setup database for Heroku postgres

--- Guitarist_Toolbox/config.py
+++ Guitarist_Toolbox/config.py
@@ -1,10 +1,9 @@
 import os
-from Guitarist_Toolbox.var import my_postgres
 
 
 class BaseConfig(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = my_postgres
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TESTING = False
 
