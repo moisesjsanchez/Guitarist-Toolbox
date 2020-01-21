999431c55f95257b1599c8bd6308b975fdc8c6ea
moisesjsanchez <moijsanchez@gmail.com>
2020-01-20 21:44:07 -0800

    Fixed CORS error with 3rd party application connecting to API

--- Guitarist_Toolbox/__init__.py
+++ Guitarist_Toolbox/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 from Guitarist_Toolbox.config import DevelopmentConfig
 
 db = SQLAlchemy()
 migrate = Migrate()
+cors = CORS()
 
 
 def create_app(config_class=DevelopmentConfig):
@@ -12,6 +14,7 @@ def create_app(config_class=DevelopmentConfig):
     app.config.from_object(config_class)
     db.init_app(app)
     migrate.init_app(app, db)
+    cors.init_app(app)
 
     from Guitarist_Toolbox.api.routes import api
     app.register_blueprint(api)
